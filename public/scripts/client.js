11b46fd416029b0a5dd41c582bf62ae5723d9167
Zobee <zohebboga@hotmail.com>
2021-05-18 23:57:25 +0000

    Implemented HTML and CSS for initial outline of the tweet element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+$(document).ready(function() {
+  const timeStamp = $(".tweet footer h5")
+  timeStamp.text(timeago.format(Date.now()))
+})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,9 @@ const CHARLIMIT = 140;
 
 $(document).ready(function() {
   console.log("READY")
-  $("#tweet-text").on("keyup", function(e) {
+  $("#tweet-text").on("keydown", function(e) {
     const counter = $(this).siblings(".tweet-btn-container").children(".counter");
     const currChars = CHARLIMIT - $(this).val().length;
-    let colorClass = currChars < 0 ? "red-text" : "";
-    counter.text(currChars).addClass(colorClass);
+    currChars < 0 ? counter.text(currChars).addClass("red-text") : counter.text(currChars).removeClass("red-text")
   })
 });
