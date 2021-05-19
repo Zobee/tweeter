09c4b88634f870172f5b131dfa0c27caa040954f
Zobee <zohebboga@hotmail.com>
2021-05-19 14:16:42 +0000

    Created createTweetElement and renderTweets functions in client.js to dynamically render tweets using template literals

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,55 @@
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+const createTweetElement = (tweetObj) => {
+  const $tweet = $(`
+  <article class="tweet">
+    <header>
+      <div>
+        <img src="${tweetObj.user.avatars}"> 
+        <h3>${tweetObj.user.name}</h3>
+      </div>
+      <h3>${tweetObj.user.handle}</h3>
+    </header>
+    <h2>${tweetObj.content.text}</h2>
+    <footer>
+      <h5>${timeago.format(tweetObj.created_at)}</h5>
+      <div class="icon-container">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+</article>
+`);
+  return $tweet
+}
+
+const renderTweets = (tweets) => tweets.map(tweet => $('#tweet-container').append(createTweetElement(tweet)));
+
 $(document).ready(function() {
-  const timeStamp = $(".tweet footer h5")
-  timeStamp.text(timeago.format(Date.now()))
+  renderTweets(data);
 })
